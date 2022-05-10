1d246ba2b044fa8ae783fea4a575b3723a17b928
Juan Pablo Avila <avilajuanp@gmail.com>
2022-05-10 12:31:10 -0300

    Initial commit

---
+++ src/test/java/com/alkemy/geoicons/geoicons/GeoiconsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.geoicons.geoicons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoiconsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
